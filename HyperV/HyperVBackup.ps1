a2a66e027a4ded098c1bcc2c3361817cbcef072b
astv25 <astv2.5@gmail.com>
2019-04-30 11:48:03 -0500

    Update HyperVBackup.ps1
    Generalized backup script, added explanitory comments for user-set variables, replaced non-functional "rd" with "Remove-Item" because this is powershell not batch.

--- HyperV/HyperVBackup.ps1
+++ HyperV/HyperVBackup.ps1
@@ -1,22 +1,21 @@
 ##Hyper-V VM Backup Utility
 ##Checks for list of VMs to back up in local file
 ##If VM is running, shuts down VM for backup and restarts
-$HyperVServerName = 'CHANGEME' ##FQDN of target Hyper-V host
-$HyperVBackupDir = 'CHANGEME' ##Full path with drive letter to desired backup folder on the Hyper-V Host
-                            ##Hyper-V does not currently support exporting to a network location
-$DestinationBackupDir = 'CHANGEME' ##SMB path with drive letter to destination backup folder. (e.g. \\hostname\e$\HyperV\)
-$TargetVMs = (Get-Content 'VMs.txt')
+$HyperVServerName = 'ChangeMe' ##FQDN of target Hyper-V host
+$HyperVBackupDir = 'ChangeMe' ##SMB path to desired backup folder on the Hyper-V Host
+$DestinationBackupDir = 'ChangeMe' ##SMB path to destination backup folder. (e.g. \\hostname\e$\HyperV\)
+$TargetVMs = (Get-Content 'VMs.txt') ##File in the same directory that contains VM names on separate lines (Names as seen in Get-VM)
 $Start = (Get-Date)
 $NumberofVMs = ($TargetVMs).count
-Write-Host "Beginning backup at $Start"
 Write-Host "$NumberofVMs VM to back up."
 Write-Host "Removing existing backup data..."
-Remove-Item -Path . -Include *.txt
+If (not exist $DestinationBackupDir\Backups) { mkdir $DestinationBackupDir\Backups }
 ForEach ($folder in Get-ChildItem $DestinationBackupDir\Backups) { Remove-Item -path $DestinationBackupDir\Backups\$folder -recurse }
 Write-Host "Done."
 Write-Host "Prompting for credentials to initiate Hyper-V connection..."
 $Credentials = (Get-Credential)
 Write-Host "Conencting to Hyper-V system..."
+Write-Host "Beginning backup: $Start"
 Enter-PSSession -ComputerName $HyperVServerName -Credential $Credentials
 Write-Host "Beginning backup job..."
 ForEach ($VM in $TargetVMs) {
@@ -30,13 +29,13 @@ Write-Host "Beginning backup of $VM..."
 Export-VM -Name $VM -Path $HyperVBackupDir\ -ComputerName $HyperVServerName
 $SourcePath = "$HyperVBackupDir\$VM"
 robocopy $SourcePath $DestinationBackupDir\Backups\$VM /MIR /NP /ETA
-rd /s /q $SourcePath
+Remove-Item -Recurse $SourcePath
 Write-Host "Backup of $VM complete."
 if ($wasrunning -eq $true) {
 Start-VM -Name $VM -ComputerName $HyperVServerName
 }
 }
-Write-Host "Disconnecting from Hyper-V server..."
+Write-Host "Disconnecting from Hyper-V server."
 Exit-PSSession
 $End = (Get-Date)
 Write-Host "Backup complete. $End"
